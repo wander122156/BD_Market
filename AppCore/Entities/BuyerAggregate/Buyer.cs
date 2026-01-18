b46ea37ff023affffabb2a473ba65520e1eae12d
wander122156 <sov48656@mail.ru>
2026-01-18 19:18:12 +0300

    Добавил сущность покупателя

---
+++ AppCore/Entities/BuyerAggregate/Buyer.cs
@@ -0,0 +1,23 @@
+using AppCore.Interfaces;
+
+namespace AppCore.Entities.BuyerAggregate;
+
+public class Buyer : BaseEntity, IAggregateRoot
+{
+    // связывает доменную сущность Buyer с аккаунтом пользователя в системе аутентификации
+    public string IdentityGuid { get; private set; }
+    
+    private List<PaymentMethod> _paymentMethods = [];
+    
+    public IEnumerable<PaymentMethod> PaymentMethods => _paymentMethods.AsReadOnly();
+    
+    // EF Core нужен конструктор без параметров для создания объекта при загрузке из базы данных
+    #pragma warning disable CS8618 // Required by Entity Framework
+    private Buyer() { }
+
+    public Buyer(string identity) : this()
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(identity);
+        IdentityGuid = identity;
+    }
+}
