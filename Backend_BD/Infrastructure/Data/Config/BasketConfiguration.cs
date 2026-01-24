dd8d1cf2a38e3e6a423a9abfe4b6ef633b137ae5
wander122156 <sov48656@mail.ru>
2026-01-24 18:45:59 +0300

    Добавил конфигурации Корзины и ее товара

---
+++ Backend_BD/Infrastructure/Data/Config/BasketConfiguration.cs
@@ -0,0 +1,18 @@
+using Backend_BD.AppCore.Entities.BasketAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Backend_BD.Infrastructure.Data.Config;
+
+public class BasketConfiguration: IEntityTypeConfiguration<Basket>
+{
+    public void Configure(EntityTypeBuilder<Basket> builder)
+    {
+        var navigation = builder.Metadata.FindNavigation(nameof(Basket.Items));
+        navigation?.SetPropertyAccessMode(PropertyAccessMode.Field);
+
+        builder.Property(b => b.BuyerId)
+            .IsRequired()
+            .HasMaxLength(256);
+    }
+}
---
+++ Backend_BD/Infrastructure/Data/Config/BasketItemConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Backend_BD.AppCore.Entities.BasketAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Backend_BD.Infrastructure.Data.Config;
+
+public class BasketItemConfiguration : IEntityTypeConfiguration<BasketItem>
+{
+    public void Configure(EntityTypeBuilder<BasketItem> builder)
+    {
+        builder.Property(bi => bi.UnitPrice)
+            .IsRequired(true)
+            .HasColumnType("decimal(18,2)");
+    }
+}
