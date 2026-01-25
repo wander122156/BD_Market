10ee3d6843e0e32d8740c893dfa91fea18ddad2f
wander122156 <sov48656@mail.ru>
2026-01-25 15:56:55 +0300

    Добавил Endpoint получения User по UserName

---
+++ Backend_BD/WebApi/UserManagementEndpoints/UserGetByUserNameEndpoint.GetByUserNameUserRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend_BD.WebApi.UserManagementEndpoints;
+
+public class GetByUserNameUserRequest : BaseRequest
+{
+    public string UserName { get; init; }
+
+    public GetByUserNameUserRequest(string userName)
+    {
+        UserName = userName;
+    }
+}
--- Backend_BD/WebApi/UserManagementEndpoints/UserGetByUserNameEndpoint.cs
+++ Backend_BD/WebApi/UserManagementEndpoints/UserGetByUserNameEndpoint.cs
@@ -1,38 +1,37 @@
-using Backend_BD.Infrastructure.Identity;
+﻿using Backend_BD.Infrastructure.Identity;
 using Backend_BD.Infrastructure.Identity.Constants;
 using Backend_BD.WebApi.Extensions;
 using Backend_BD.WebApi.UserManagementEndpoints.Models;
 using FastEndpoints;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Identity;
 
 namespace Backend_BD.WebApi.UserManagementEndpoints;
 
-public class UserGetByIdEndpoint (UserManager<ApplicationUser> userManager) 
-    : Endpoint <GetByIdUserRequest, GetUserResponse>
+public class UserGetByUserNameEndpoint (UserManager<ApplicationUser> userManager) : Endpoint <GetByUserNameUserRequest, GetUserResponse>
 {
     public override void Configure()
     {
-        Get("api/users/{userId}");
+        Get("api/users/name/{userName}");
         Roles(AuthorizationConstants.Roles.ADMINISTRATORS);
         AuthSchemes(JwtBearerDefaults.AuthenticationScheme);
         Description(d =>
             d.Produces<GetUserResponse>()
-                .WithTags("UserManagementEndpoints"));
+            .WithTags("UserManagementEndpoints"));
     }
 
-    public override async Task HandleAsync(GetByIdUserRequest request, CancellationToken ct)
+    public override async Task HandleAsync(GetByUserNameUserRequest request, CancellationToken ct)
     {
         var response = new GetUserResponse(request.CorrelationId());
 
-        ApplicationUser? userResponse = await userManager.FindByIdAsync(request.UserId);
+        var userResponse = await userManager.FindByNameAsync(request.UserName);
         if (userResponse is null)
         {
-           await Send.NotFoundAsync(ct);
-           return;
+            await Send.NotFoundAsync(ct);
+            return;
         }
         response.User = userResponse.ToUserDto();
-        
         await Send.OkAsync(response, ct);
     }
 }
